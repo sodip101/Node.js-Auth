fb3fdf4df35bc5c978ca0a8a5ef63aa6dfb5ee15
Sodip Bikram Thapa <sodip99@gmail.com>
2020-09-07 10:09:29 +0545

    added password hashing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const {isEmail}=require('validator');
+const bcrypt=require('bcrypt');
 
 const userSchema=new mongoose.Schema({
     email:{
@@ -15,5 +16,18 @@ const userSchema=new mongoose.Schema({
         minlength:[6,'Please enter a password of minimum length 6']
     }
 });
+
+//password hashing using mongoose 'pre' hook
+userSchema.pre('save',async function(next){
+    const salt=await bcrypt.genSalt();
+    this.password=await bcrypt.hash(this.password,salt);
+    next();
+});
+
+//mongoose 'post' hook
+// userSchema.post('save',function(doc,next){
+//     console.log('new user added',doc);
+//     next();
+// });
 
 module.exports=new mongoose.model('User',userSchema);
