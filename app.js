9cabccf557b10b0170523cdce25580a23490d729
Sodip Bikram Thapa <sodip99@gmail.com>
2020-09-08 09:45:57 +0545

    added examples for cookies and fixed nodemon

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes=require('./routes/authRoutes.js');
+const cookieParser=require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,3 +24,21 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.use(authRoutes);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+//cookies
+//set cookies
+app.get('/set-cookies',(req,res)=>{
+  res.cookie('newUser', true,{
+    maxAge:1000*60*60*24,
+    httpOnly:true
+  });
+
+  res.send('cookie has been set');
+});
+
+//read cookies
+app.get('/read-cookies',(req,res)=>{
+  const cookies=req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
